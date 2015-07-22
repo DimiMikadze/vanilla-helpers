7b42eaed7763bb6425e49f57123dcffdb41db5db
Ditmens <dimitrimikadze@gmail.com>
2015-07-23 00:02:16 +0400

    upload

---
+++ readme.md
@@ -0,0 +1,197 @@
+# Vanilla Javascript Helper Functions, Ajax, Effects, DOM manipulation, Styles, Utilities. 
+
+Easy to use Vanilla Javascript Helper Functions Collection, Ajax, Effects, DOM manipulation, Styles, Utilities ...
+
+## Features
+
+- Lightweight, No libraries required
+- Ajax
+- Effects
+- DOM manipulation 
+- Styles
+- Utilities
+
+## Bower
+
+````
+bower install vanilla-helpers
+````
+
+## Getting Started
+
+include vanillaHelpers.js OR vanillaHelpers.min.js
+
+
+## Some Examples
+
+
+## Example Ajax GET
+
+````
+vanillaHelpers.ajaxGet('http://example.com/fa=foo&foo=faa', function(data) {
+	console.log(data);
+});
+````
+
+If server returns json, parse it in callback function
+
+````
+vanillaHelpers.ajaxGet('http://example.com/fa=foo&foo=faa', function(data) {
+	console.log(JSON.parse(data));
+});
+````
+
+## Example Ajax Post
+
+````
+vanillaHelpers.ajaxPost('http://example.com/', 'a=b&b=c', function(data) {
+	console.log(data);
+});
+````
+
+You can also pass object as data
+
+````
+vanillaHelpers.ajaxPost('http://example.com/', { a: 'b', b: 'c' }, function(data) {
+	console.log(data);
+});
+````
+
+## Fade In
+
+````
+vanillaHelpers.fadeIn(document.getElementById('test'), 3000);
+
+````
+
+With Callback
+
+````
+vanillaHelpers.fadeIn(document.getElementById('test'), 3000, function() {
+	console.log('animation stopped');
+});
+
+````
+
+## Fade Out
+
+````
+vanillaHelpers.fadeOut(document.getElementById('test'), 3000);
+
+````
+
+With Callback
+
+````
+vanillaHelpers.fadeOut(document.getElementById('test'), 3000, function() {
+	console.log('animation stopped');
+});
+
+````
+
+## Next Element
+
+````
+vanillaHelpers.nextElement(document.getElementById('test'));
+````
+
+## Previous Element
+
+````
+vanillaHelpers.previousElement(document.getElementById('test'));
+````
+
+## Browser Vendor Properties
+
+````
+vanillaHelpers.setVendor(document.getElementById('test'), 'Transition', '1s');
+
+````
+
+Will add next styles
+
+````
+-webkit-transition: 1s;
+   -moz-transition: 1s;
+    -ms-transition: 1s;
+     -o-transition: 1s;
+        transition: 1s;
+````
+
+## Generate random integer
+
+````
+vanillaHelpers.randomInt(1, 10);
+````
+
+## Get Current url segment
+
+if url is: http://example.com/faa/foo/boo
+
+````
+vanillaHelpers.urlSegment(2);
+````
+
+Will output foo
+
+## Check if element hass specific class
+
+````
+vanillaHelpers.hasClass(document.getElementById('test'), 'foo');
+````
+
+## Add Class
+
+````
+vanillaHelpers.addCls(document.getElementById('test'), 'foo');
+````
+
+## Remove Class
+
+````
+vanillaHelpers.removeCls(document.getElementById('test'), 'foo');
+````
+
+## Toggle Class
+
+````
+vanillaHelpers.toggleCls(document.getElementById('test'), 'foo');
+````
+
+## Matches
+
+````
+vanillaHelpers.matches(document.getElementById('selector'), '#selector');
+````
+
+## String Starts With
+
+````
+vanillaHelpers.startsWith('SomeRandomText', 'Some');
+````
+
+## String Ends With
+
+````
+vanillaHelpers.endsWith('SomeRandomText', 'Some');
+````
+
+## String Between two characters
+
+````
+vanillaHelpers.stringBetween('SomeRandomText', 'R', 'T');
+````
+
+## In Array
+
+````
+vanillaHelpers.inArray(['a', 'b', 'c', 'd'], 'c');
+````
+
+## Contributing
+
+contributions are more than welcome!
+
+## License
+
+See license.txt
---
+++ vanillaHelpers.js
@@ -0,0 +1,315 @@
+/*
+ vanillaHelpers v0.1
+ (c) Dimitri Mikadze
+ https://github.com/DimitriMikadze/vanilla-helpers
+ License: MIT
+*/
+vanillaHelpers = {
+
+	/**
+	* Ajax GET request
+	*
+	* @param url 
+	* @param callback function
+	* @return response
+	*/
+	ajaxGet: function(url, callback) {
+
+	    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	    xhr.open('GET', url);
+	    xhr.onreadystatechange = function() {
+	        if (xhr.readyState > 3 && xhr.status === 200) {
+	        	callback(xhr.responseText);
+	        }
+	    };
+	    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	    xhr.send();
+
+	    return xhr;
+	},
+
+	/**
+	* Ajax POST request
+	*
+	* @param url post to
+	* @param data send to
+	* @param callback function
+	* @return response
+	*/
+	ajaxPost: function(url, data, callback) {
+
+	    var params = typeof data == 'string' ? data : Object.keys(data).map(
+	            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+	        ).join('&');
+
+	    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+	    xhr.open('POST', url);
+	    xhr.onreadystatechange = function() {
+	        if (xhr.readyState > 3 && xhr.status === 200) { 
+	        	callback(xhr.responseText); 
+	        }
+	    };
+	    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	    xhr.send(params);
+
+	    return xhr;
+	},
+
+	/**
+	* Find next element
+	*
+	* @param element
+	* @return next element
+	*/
+	nextElement: function(element) {
+
+	    do {
+	        element = element.nextSibling;
+	    } while (element && element.nodeType !== 1);
+
+	    return element;        
+	},
+
+	/**
+	* Find previous element
+	*
+	* @param element
+	* @return next element
+	*/
+	previousElement: function(element) {
+
+	    do {
+	        element = element.previousSibling;
+	    } while (element && element.nodeType !== 1);
+
+	    return element;  
+	},
+
+	/**
+	* Fade in animation
+	*
+	* @param element  
+	* @param animation speed 
+	* @param callback function
+	*/
+	fadeIn: function(element, speed, callback) {
+
+		( ! element.style.opacity) ? element.style.opacity = 0 : '';
+
+		var changeOpacity = setInterval(function() {
+			
+			element.style.opacity = Number(element.style.opacity) + 0.02;
+			if(element.style.opacity >= 1) {
+				clearInterval(changeOpacity);
+				if (callback && typeof(callback) === "function") {
+	        		callback();
+	    		}
+			}
+		}, speed / 50);
+	},
+
+	/**
+	* Fade Out animation
+	*
+	* @param element  
+	* @param animation speed 
+	* @param callback function
+	*/
+	fadeOut: function(element, speed, callback) {
+
+		( ! element.style.opacity) ? element.style.opacity = 1 : '';
+
+		var changeOpacity = setInterval(function() {
+
+			element.style.opacity -= 0.02;
+			if(element.style.opacity <= 0) {
+				clearInterval(changeOpacity);
+				if (callback && typeof(callback) === "function") {
+	        		callback();
+	    		}
+			}
+
+		}, speed / 50);
+	},
+
+	/**
+	* set browser vendor properties
+	*
+	* @param element  
+	* @param property CSS3 set of specifications e.g Transition 
+	* @param value CSS3 set of specifications e.g Transition speed '1s'
+	*/
+	setVendor: function(element, property, value) {
+
+		element.style["webkit" + property] = value;
+		element.style["Moz" + property] = value;
+		element.style["ms" + property] = value;
+		element.style["o" + property] = value;
+	},
+
+	/**
+	* Generate Random Integer
+	*
+	* @param min value of integer
+	* @param max value of integer
+	* @return integer
+	*/
+	randomInt: function(min, max) {
+
+    	return Math.floor(Math.random() * (max - min + 1) + min);
+	},
+
+	/**
+	* Get current url segment
+	*
+	* @param index of url integer
+	* @return string url segment
+	*/
+	urlSegment: function(index) {
+		
+		var url = window.location.pathname.split( '/' );
+		return url[index];
+	},
+
+	/**
+	* Check if element contains class
+	*
+	* @param element
+	* @param class
+	* @return boolean
+	*/
+	hasClass: function(element, className) {
+		
+    	return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -  1;
+	},
+
+	/**
+	* Add Class
+	*
+	* @param element
+	* @param className to add
+	*/
+	addCls: function(element, className) {
+
+		if (element.classList) {
+  			element.classList.add(className);
+  		} else {
+  			element.className += ' ' + className;
+  		}
+	},
+
+	/**
+	* Remove Class
+	*
+	* @param element
+	* @param className to remove
+	*/
+	removeCls: function(element, className) {
+		
+		if (element.classList) {
+  			element.classList.remove(className);
+  		} else {
+  			element.className = element.className.replace(new RegExp(className, "g"), "");
+  		}
+
+	},
+
+	/**
+	* Toggle class
+	*
+	* @param element
+	* @param className to toggle
+	*/
+	toggleCls: function(element, className) {
+		
+		if (element.classList) {
+			element.classList.toggle(className);
+		} else {
+			var classes = element.className.split(' ');
+			var existingIndex = classes.indexOf(className);
+
+			if (existingIndex >= 0)
+			classes.splice(existingIndex, 1);
+			else
+			classes.push(className);
+
+			el.className = classes.join(' ');
+		}	
+		
+	},
+
+	/**
+	* Matches
+	*
+	* @param element
+	* @param selector
+	* @return boolean
+	* e.g matches(document.getElementById('selector'), '#selector')
+	*/
+	matches: function(element, selector) {
+
+	  	return (element.matches || 
+	  			element.matchesSelector || 
+	  			element.msMatchesSelector || 
+	  			element.mozMatchesSelector || 
+	  			element.webkitMatchesSelector || 
+	  			element.oMatchesSelector).call(element, selector);
+	},
+
+	/**
+	* String starts with
+	* 
+	* @param string
+	* @param string suffix 
+	* @return boolean
+	*/
+	startsWith: function(string, suffix) {
+
+		return (string.substring(0, suffix.length) === suffix);
+
+	},
+
+	/**
+	* String ends with
+	* 
+	* @param string
+	* @param string suffix 
+	* @return boolean
+	*/
+	endsWith: function(string, suffix) {
+
+    	return string.indexOf(suffix, string.length - suffix.length) !== -1;
+	},
+
+	/**
+	* String between two characters
+	*
+	* @param string
+	* @param start first character
+	* @param end second character
+	* @return string
+	*/
+	stringBetween: function(string, start, end) {
+
+		return string.substring(string.lastIndexOf(start) + 1, string.lastIndexOf(end));
+	},
+
+	/**
+	* In Array
+	*
+	* @param array
+	* @param value check to
+	* @return boolean
+	*/
+	inArray: function(array, value) {
+
+	    for(var i = 0; i < array.length; i++) {
+	        if(array[i] === value) {
+	        	return true;
+	        }
+	    }
+    	return false;
+	}
+
+}
